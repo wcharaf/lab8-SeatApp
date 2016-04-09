2d7d3e0f42994d7508b8ed2f637b9f1f3137bca8
Wissam Charafeddine <wissam.charafeddine@gmail.com>
2016-04-09 10:16:09 -0400

    WissamSAT

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-$(document).ready(function(){ // JavaScript-y things here
+$(document).ready(function(){
 
   $(".col-md-3").click(function(){
-      alert("The paragraph was clicked.");
+    function popup() {
+        $("#contactdiv").css("display", "block");
+        }
+        $("#cancel").click(function() {
+          $(this).parent().parent().hide();
+        });
+        $("#submit").click(function() {
+          var name = $("#name").val();
+          var email = $("#email").val();
+          var contact = $("#contactno").val();
+          if (name == "" || email == "" || contactno == ""){
+            alert("Please Fill All Fields");
+          }else {
+            if (validateEmail(email)) {
+              $("#contactdiv").css("display", "none");
+            }else {
+              alert('Invalid Email Address');
+            };
+
+            function validateEmail(email) {
+              var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+              if (filter.test(email)) {
+                return true;
+              }else {
+                return false;
+              }
+            }
+          }
     });
-
-
-});
+      // alert("The paragraph was clicked.");
+      // $("#contactdiv").css("display", "block");
+    // });
