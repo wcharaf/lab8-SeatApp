e5d7ac66fa3f2a7548b104864ce7ba401b411345
Aleah Ellis <aleah.is@icloud.com>
2016-04-09 09:34:26 -0400

    see app.js file for idea

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ $(document).ready(function(){ // JavaScript-y things here
 
 
 });
+//using the $(<>).data("{object:object, object: object}")
+//also use getData to bring data from the form and displa as an object with key value pairs
+//in a var= {}
+//which we can applend to the selected seats
+//that takes care of the bonus
+//http://tutorialzine.com/2010/11/jquery-data-method/
