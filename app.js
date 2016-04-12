ab263a01e1abb34d128cc9dabaad8a292873bec2
Cara Canady <cara.canady@gmail.com>
2016-04-11 23:14:59 -0400

    working form and connected clicked box with form

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
 $(document).ready(function(){
 
-// var reservationObj = {};
+var seat = 'Seat99';
 
-    // function popup() {
-    //     $("#contactdiv").css("display", "block");
-    //     }
-        // $("#cancel").on("click",function() {
-        //   $(this).parent().parent().hide();
-        // });
-        $("#submit").on("click",function() {
-          var name = $("#name").val();
-          var email = $("#email").val();
-          var contact = $("#contactno").val();
-          console.log(name+email+contact);
-          // if (name === "" || email === "" || contactno === ""){
-          //   alert("Please Fill All Fields");
-          // }else {
-          //   if (validateEmail(email)) {
-          //     $("#contactdiv").css("display", "none");
-          //   }else {
-          //     alert('Invalid Email Address');
-          //   };
-          //
-          //   function validateEmail(email) {
-          //     var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-          //     if (filter.test(email)) {
-          //       return true;
-          //     }else {
-          //       return false;
-          //     }
-          //   }
-          // }
-        });
-    // $(".col-md-3").on("click", function(){
-
-    // });
+  $(".col-md-3").click(function(){
+    $(this).css("background-color", "black");
+    $(this).css("color", "white");
+    seat = $(this).attr('id');
   });
-      // alert("The paragraph was clicked.");
-      // $("#contactdiv").css("display", "block");
-    // });
+
+  $("#submit").on("click",function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var contact = $("#contactno").val();
+    
+    // console.log(name+email+contact);
+    alert("Hello, " + name + ".  Your phone number is " + contact + " and your email is " + email + ".  Thank you for reserving " + seat + ".");
+  });
+  
+          
+    // if (name === "" || email === "" || contactno === ""){
+    //   alert("Please Fill All Fields");
+    //     }else {
+    //       if (validateEmail(email)) {
+    //         $("#contactdiv").css("display", "none");
+    //       }else {
+    //           alert('Invalid Email Address');
+    //         };
+          
+    //         function validateEmail(email) {
+    //           var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+    //           if (filter.test(email)) {
+    //             return true;
+    //           }else {
+    //             return false;
+    //           }
+    //         }
+    //       }
+    //     });
+  
+
+  });
+   
